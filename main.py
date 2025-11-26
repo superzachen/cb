3ca511f8d86c7d09ca82e51f60140b8181de1962
Zach Chen <zachary.zh.chen@gmail.com>
2025-11-25 20:39:55 -0800

    working on Chckerboard cipher

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+import itertools
 import typer  # noqa: F401
 app = typer.Typer()
 
@@ -67,6 +69,47 @@ def atbash(input: str) -> str:
         change = ord(i) - ord('a')
         output.append(chr(ord('z') - change))
     print(''.join(output))
+
+@app.command()
+def cb55(input:str, pob_key:str) -> str:
+    input = input.lower()
+    pob_key = pob_key.lower()
+    board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0] ]
+    letters = [char for char in string.ascii_lowercase if char != 'j']
+    pob_len = len(pob_key)
+    for r in range(5):
+        for c in range(5):
+            if pob_len > 0:
+                board[r][c] = pob_key[0]
+                letters.remove(pob_key[0])
+                pob_len -= 1
+                pob_key = pob_key[1:]
+            else:
+                board[r][c] = letters[0]
+                letters.pop(0)
+    words = input.split(' ')
+    rk_set = set()
+    ck_set = set()
+    token_list = []
+    for word in words:
+        for i in range(0, len(word), 2):
+            token_list.append(word[i:i+2])
+    for token in token_list:
+        rk_set.add(token[0])
+        ck_set.add(token[1])
+    rk_combos = ["".join(p) for p in itertools.permutations(list(rk_set))]
+    ck_combos = ["".join(p) for p in itertools.permutations(list(ck_set))]
+    dict_set = load_eng_dict()
+    rk_words = []
+    ck_words = []
+    for combo in rk_combos:
+        if combo in dict_set:
+            rk_words.append(combo)
+    for combo in ck_combos:
+        if combo in dict_set:
+            ck_words.append(combo)
+    print(ck_set, rk_set)
+
 
 if __name__ == "__main__":
     app()
